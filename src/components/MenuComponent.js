8b39d62a72bb9a80e9dfdbad99dd19bacfaa3b41
harsh123-baba <harshitgoyal213@gmail.com>
2022-12-24 11:18:17 +0530

    changes done

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+class Menu extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+    render(){
+        const  menu = this.props.dishes.map((dish)=>{
+            return (
+                <div key = {dish.id} className="col-12 col-md-5 m-1">
+                     <Card tag = "li">
+                        
+                        <CardImg width = "100%" object src = {dish.image} alt = {dish.name} />
+                        
+                        <CardImgOverlay body className="ml-5">
+                            <CardTitle heading>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardImgOverlay>
+                     </Card>
+                </div>
+            );
+        });
+
+        return (
+            <div className='container'>
+                <div className='row'>
+                        {menu}
+                </div>
+            </div>
+        );
+    }
+}
+export default Menu;
